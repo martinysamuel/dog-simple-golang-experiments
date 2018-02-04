6cde36290645842ee2d6cdfd188218f3ef720e8e
Martin <tin@kemmart.com>
2018-02-04 13:17:59 -0600

    Updates.

--- simple_database_login/main.go
+++ simple_database_login/main.go
@@ -1,61 +1,78 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"database/sql"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"html/template"
 	"log"
 	"net/http"
-	"strings"
-	"crypto/sha256"
-	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
-	_ "src/me/simple_database_login/helpers.go"
 )
-const BlockSize = 64
 
-type User struct {
-	id int
-	email    string
-	password string
-}
-
-
-
+var (
+	email   string
+	id      int
+	hash    string
+	message string
+)
 
 func do_index(RESPONSE http.ResponseWriter, REQUEST *http.Request) {
-
-	REQUEST.ParseForm()
-
-	fmt.Println(REQUEST.Form["url_long"])
-
-	for k, v := range REQUEST.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-
-	}
+	TEMPLATE, _ := template.ParseFiles("./views/login.html")
+	TEMPLATE.Execute(RESPONSE, nil)
 }
 
 func do_login(RESPONSE http.ResponseWriter, REQUEST *http.Request) {
-	fmt.Println("method:", REQUEST.Method) //get request method
+
+	//fmt.Println("Login function has been hit.")
+
+	const BlockSize = 64
+
+	//fmt.Println("method:", REQUEST.Method) //get request method
 	if REQUEST.Method == "GET" {
-		TEMPLATE, _ := template.ParseFiles("views/do_login.html")
-		TEMPLATE.Execute(RESPONSE, nil)
+
+		message = "Method issue. We should probably do a redirect on this piece. I'll do that in the next one."
+		out := bytes.NewBufferString(message)
+		TEMPLATE, _ := template.ParseFiles("./views/error.html")
+		TEMPLATE.Execute(RESPONSE, out)
+
 	} else {
+
+		//We have a post state! Excellent.
+
+		DATABASE, err := sql.Open("sqlite3", "./data/idium.db")
+		do_error_checking(err)
 		REQUEST.ParseForm()
 
-		//Let's make us some strings that we can use and pass out.
-		
+		email = REQUEST.FormValue("email")
+		hash = hash_password(REQUEST.FormValue("password"))
 
-		// logic part of log in
-		fmt.Println("email:", REQUEST.Form["email"][0])
-		fmt.Println("password:", REQUEST.Form["password"][0])
+		var statement string = fmt.Sprintf("SELECT id,email FROM users where email='%s' and password ='%s'", email, hash)
 
-		email := REQUEST.Form["email"][0]
-		hash := hash_password(REQUEST.Form["password"][0])
-		fmt.Println(hash)
+		data := DATABASE.QueryRow(statement, 3)
 
-		TEMPLATE, _ := template.ParseFiles("views/secure.html")
-		TEMPLATE.Execute(RESPONSE, REQUEST.Form)
+		//Here, we can't take the shortcut with the easy error checking,
+		//because we can have a variety of errors here. So we switch it.
+
+		switch err := data.Scan(&id, &email); err {
+		case sql.ErrNoRows:
+			//No such user.
+			message = ("Not a valid user, sorry. Please try again.")
+			out := bytes.NewBufferString(message)
+			TEMPLATE, _ := template.ParseFiles("./views/error.html")
+			TEMPLATE.Execute(RESPONSE, out)
+		case nil:
+			//Valid user
+			TEMPLATE, _ := template.ParseFiles("./views/secure.html")
+			TEMPLATE.Execute(RESPONSE, REQUEST.Form)
+		default:
+			//Method or database issue.
+			message = "Probably a database issue."
+			out := bytes.NewBufferString(message)
+			TEMPLATE, _ := template.ParseFiles("./views/error.html")
+			TEMPLATE.Execute(RESPONSE, out)
+		}
 
 	}
 }
@@ -68,8 +85,19 @@ func main() {
 	http.HandleFunc("/login", do_login)
 	err := http.ListenAndServe(":4000", nil) // setting listening port
 	if err != nil {
-		//TEMPLATE, _ := template.ParseFiles("views/error.html")
-		//TEMPLATE.Execute(err, nil)
 		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+//Helper functions, here, for now.
+
+func hash_password(password string) string {
+	sum := sha256.Sum256([]byte(password))
+	out := fmt.Sprintf("%x", sum)
+	return out
+}
+func do_error_checking(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
